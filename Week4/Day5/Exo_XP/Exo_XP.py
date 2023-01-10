18267c91331c375b58703bf1cf18e6371460a2f1
THIOMBIANO <simon.thiombiano@gmail.com>
2023-01-10 20:38:31 +0000

    Mini Projet Tic Tack Toe

--- Week4/Day5/Exo_XP/Exo_XP.py
+++ Week4/Day5/Exo_XP/Exo_XP.py
@@ -1,11 +1,4 @@
 
-
-
-
-
-
-
-
 M=[
     [' ',' ',' '],[' ',' ',' '],[' ',' ',' ']
 ]
@@ -21,6 +14,7 @@ def display_board():
 
 def display_play(liste,x):
     #if player1:
+    
     z=int(input("entrer la ligne:"))
     while z>3:
         print("entrer un nombre compris entre 1 et 3 !")
@@ -29,14 +23,29 @@ def display_play(liste,x):
     while y>3:
         print("entrer un nombre compris entre 1 et 3 !")
         y=int(input("entrer la colonne:"))
-    if liste[z-1][y-1]==" ":
-        liste[z-1][y-1]=x
+    
+    while liste[z-1][y-1]!=" ":
+        print("Reprenez la case est occupée")
+        z=int(input("entrer la ligne:"))
+        while z>3:
+            print("entrer un nombre compris entre 1 et 3 !")
+            z=int(input("entrer la ligne:"))
+        y=int(input("entrer la colonne:"))
+        while y>3:
+            print("entrer un nombre compris entre 1 et 3 !")
+            y=int(input("entrer la colonne:"))
+        if liste[z-1][y-1]==" ":
+            liste[z-1][y-1]=x
+            break
     else:
-        print("cette case est deja rempli" )
+        liste[z-1][y-1]=x
+
+       
     return liste
 
 
 def verifier(liste):
+    
     if liste[0][0]==liste[0][1]==liste[0][2]=="x":
        
         print("le joueur x gagne !")
@@ -62,60 +71,86 @@ def verifier(liste):
     elif liste[2][0]==liste[2][1]==liste[2][2]=="o":
         
         print("le joueur o gagne !")
+        exit()
        
     elif liste[0][0]==liste[1][0]==liste[2][0]=="x":
        
         print("le joueur x gagne !")
-       
+        exit()
     elif liste[0][0]==liste[1][0]==liste[2][0]=="o":
         
         print("le joueur o gagne !")
-       
+        exit()
     elif liste[0][1]==liste[1][1]==liste[2][1]=="x":
        
         print("le joueur x gagne !")
-       
+        exit()
     elif liste[0][1]==liste[1][1]==liste[2][1]=="o":
         
         print("le joueur o gagne !")
-       
+        exit()
     elif liste[0][2]==liste[1][2]==liste[2][2]=="x":
         
         print("le joueur x gagne !")
-       
+        exit()
     elif liste[0][2]==liste[1][2]==liste[2][2]=="o":
         
         print("le joueur x gagne !")
        
     elif liste[0][0]==liste[1][1]==liste[2][2]=="x":
        print("le joueur x gagne ")
-       
+       exit()
     elif liste[0][0]==liste[1][1]==liste[2][2]=="o":
        print("le joueur o gagne ")
-       
+       exit()
     elif liste[2][0]==liste[1][1]==liste[0][2]=="x":
        print("le joueur x gagne ")
-       
+       exit()
     elif liste[2][0]==liste[1][1]==liste[0][2]=="o":
        print("le joueur o gagne ")
-       
-    else:
-        print("egalité")
+       exit()
+   
        
 def play():
-    for i in range(4):
-        
-        print("player1:")
-        display_board()
-        display_play(M,"x")
-        display_board() 
-        
-        print("player2:") 
-        display_board()
-        display_play(M,"o")
-        display_board()
-    verifier(M)
+    print("                  ")
+    print("Bienvenue sur Tic Tack Toe")
+    print( "                   ")
+    i=0
+    cpt=1
+    while i<=5:
+        if not verifier(M):
             
+            
+            display_board()
+            print("           ")
+            print("player1:")
+            display_play(M,"x")
+            display_board()
+            cpt+=1
+            if cpt==10:
+                if verifier(M):
+                    exit()
+                else:
+                    print("égalité")
+                    exit()
+             
+            display_board()
+            print("               ")
+            print("player2:")
+            display_play(M,"o")
+            display_board()
+            #verifier(M)
+            cpt+=1
+            if cpt==11:
+                print("égalité")
+                exit()  
+        elif verifier(M):
+            exit()
+        
+          
+            
+        i+=1
+        
        
         
 
